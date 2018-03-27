9807bc7875c9224755f511713e8bb23c74ac9a0f
Gareth <contact@garethjeanne.co.uk>
2018-03-27 22:19:42 +0100

    make sed work

--- deploy_g8s.sh
+++ deploy_g8s.sh
@@ -1,7 +1,12 @@
 # Build and push the image
 #!/bin/bash
-sed -e "s/\${PROJECT_ID}/$GCLOUD_PROJECT/" -e "s/\${PRIVATE_KEY_ID}/$PRIVATE_KEY_ID/" -e "s/\${PRIVATE_KEY}/$PRIVATE_KEY/" -e "s/\${CLIENT_EMAIL}/$CLIENT_EMAIL/" -e "s/\${CLIENT_ID}/$CLIENT_ID/" -e "s/\${CERT_URL}/$CERT_URL/" /base/gcloud_key.json
-gcloud auth activate-service-account --key-file=/base/gcloud_key.json
+sed -i -e "s/{PROJECT_ID}/$GCLOUD_PROJECT/" gcloud_key.json
+sed -i -e "s/{PRIVATE_KEY_ID}/$PRIVATE_KEY_ID/" gcloud_key.json
+sed -i -e "s/{PRIVATE_KEY}/$PRIVATE_KEY/" gcloud_key.json
+sed -i -e "s/{CLIENT_EMAIL}/$CLIENT_EMAIL/" gcloud_key.json
+sed -i -e "s/{CLIENT_ID}/$CLIENT_ID/" gcloud_key.json
+sed -i -e "s/{CERT_URL}/$CERT_URL/" gcloud_key.json
+gcloud auth activate-service-account --key-file=gcloud_key.json
 gcloud container clusters get-credentials $GCLOUD_CLUSTER --zone $GCLOUD_ZONE --project $GCLOUD_PROJECT
 
 kubectl set image deployment/$GCLOUD_DEPLOYMENT $GCLOUD_CONTAINER=$GCLOUD_IMAGE:$BUILDKITE_COMMIT
